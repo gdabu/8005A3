cb56770d4d7a6eb8bc01407b82da91426eecdf86
gdabu <geoffdabu@gmail.com>
2015-03-12 13:25:52 -0700

    working forwarder

---
+++ Forwarder1.rb
@@ -0,0 +1,81 @@
+require "socket"
+require "logger"
+require 'thread'
+require 'thwait'
+
+require_relative "ServerFunctions.rb"
+
+#------------------------
+#-- Variable Declaration
+#------------------------
+begin
+	READBUFFERSIZE = Integer(ARGV[0])
+	$configFile = ARGV[1]
+rescue Exception => argException
+  	puts ">> Illegal Arguments"
+  	puts ">> Usage: ruby forwarder.rb (readBufferSize configFile)"
+  	exit
+end
+
+#contains all the variables needed to forward a packet from one socket to another
+forwardingPairs = Array.new{Array.new(3)}
+
+#Parse the log file for all the IP:PORT combinations, and add them to the forwardingPairs Array
+File.foreach($configFile).grep /FORWARD/ do |line|
+	forwardingInfo = line.split(/[ ,:]/)
+	forwardingPairs.push([forwardingInfo[1], forwardingInfo[2], forwardingInfo[3].strip!])
+end
+
+
+#Iterate through every forwarding combination
+for i in 0..forwardingPairs.length - 1 do
+	
+	#forwardingPairs[i][0] => listeningPort // The port which listens for connections
+	#forwardingPairs[i][1] => serverIP 		// IP of the server to which you are sending to
+	#forwardingPairs[i][2] => serverPort 	// Port of the server to which you are sending to
+
+	#Start a thread for every TCP Server Accepting Socket 
+	threads = Thread.new(forwardingPairs[i][0], forwardingPairs[i][1], forwardingPairs[i][2]) do |listeningPort_threadlocal, serverIP_threadlocal, serverPort_threadlocal|
+
+
+		puts "Starting Server on Port #{listeningPort_threadlocal}"
+		#initialize TCP Server Accepting Socket
+		clientListenerSocket = TCPServer.new( listeningPort_threadlocal )
+		clientListenerSocket.setsockopt( Socket::SOL_SOCKET, Socket::SO_REUSEADDR, 1 )
+
+		#This infinite loop allows 
+		while 1
+
+
+			Thread.new(clientListenerSocket.accept) do |clientTransmitterSocket|
+
+				puts "new connection"
+				serverTransmitterSocket = TCPSocket.open(serverIP_threadlocal, serverPort_threadlocal)
+				puts "connected to server"
+
+				while 1
+
+					sentMessage = forwardMessage(clientTransmitterSocket, serverTransmitterSocket, READBUFFERSIZE)
+					puts "rx from client tx to server"
+					sentMessage = forwardMessage(serverTransmitterSocket, clientTransmitterSocket, READBUFFERSIZE)
+					puts "rx from server tx to client"
+
+					#Client kills connection
+					if clientTransmitterSocket.eof?
+
+						clientTransmitterSocket.close
+						serverTransmitterSocket.close
+
+						break #break here to leave thread block, hence ending thread
+					end #end if
+
+				end #end while 1
+			end #end thread 
+
+		end
+
+
+	end
+end
+
+ThreadsWait.all_waits(*threads)
---
+++ ServerFunctions.rb
@@ -0,0 +1,96 @@
+#/*---------------------------------------------------------------------------------------
+#--	SOURCE FILE: 	ServerFunctions.rb - Consists of all the functions needed for basic 
+#--										 socket functions
+#--
+#--	PROGRAM:		ServerFunctions.rb
+#--					
+#--	FUNCTIONS:		Mainly just a place holder for all client function functions
+#--
+#--	DATE:			February 23, 2015
+#--
+#--	DESIGNERS:		GEOFF DABU
+#--	PROGRAMMERS:	GEOFF DABU, CHRIS HUNTER
+#--
+#--	NOTES: 			There is no need to run this file, this file is mainly required by 
+#--						- Server_SELECT.rb 
+#--						- Server_MT.rb
+#---------------------------------------------------------------------------------------*/
+
+#/*---------------------------------------------------------------------------------------
+#-- FUNCTION: 		acceptNewConnectionNonBlock
+#--
+#-- NOTES: 			accepts a new socket connection, from @arg [serverSocket] and adds it 
+#--					to @arg [connections] - a list of all current client connections. The
+#-- 				total number of connections are then printed out.
+#--					The accept call is non blocking.
+#--
+#-- USED IN: 		Server_SELECT.rb
+#---------------------------------------------------------------------------------------*/
+def acceptNewConnectionNonBlock(serverSocket, connections)
+	newSock = serverSocket.accept_nonblock() 
+	connections.push( newSock )
+	puts connections.length
+	return newSock
+end
+
+#/*---------------------------------------------------------------------------------------
+#-- FUNCTION: 		acceptNewConnectionBlock
+#--
+#-- NOTES: 			accepts a new socket connection, from @arg [serverSocket] and adds it 
+#--					to @arg [connections] - a list of all current client connections. The
+#-- 				total number of sockets in @arg [connections] is then printed out.
+#--					The accept call is blocking.
+#--
+#-- USED IN: 		N/A
+#---------------------------------------------------------------------------------------*/
+def acceptNewConnectionBlock(serverSocket, connections)
+	newSock = serverSocket.accept() 
+	connections.push( newSock )
+	puts connections.length
+	return newSock
+end
+
+#/*---------------------------------------------------------------------------------------
+#-- FUNCTION: 		killConnection
+#--
+#-- NOTES: 			Takes the connected socket @arg [clientSocket] and closes the connection.
+#--					The socket @arg [clientSocket] is then deleted from @arg [connections]
+#--					- the list of all current client connections. The total number of sockets
+#--					in @arg [connections] is then printed out.
+#--
+#-- USED IN: 		Server_SELECT.rb
+#--					Server_MT.rb
+#---------------------------------------------------------------------------------------*/
+def killConnection( clientSocket, connections )
+	clientSocket.close
+	connections.delete(clientSocket)
+	puts connections.length
+end
+
+#/*---------------------------------------------------------------------------------------
+#-- FUNCTION: 		echoMessage
+#-- 
+#-- NOTES: 			Takes the connected socket @arg [clientSocket] and reads from the input
+#--					buffer, followed by writing to the output buffer. The received data is
+#--					then printed out.
+#--
+#-- USED IN: 		Server_SELECT.rb
+#--					Server_MT.rb
+#---------------------------------------------------------------------------------------*/
+def echoMessage( clientSocket, readBufferSize )
+	data = clientSocket.read( readBufferSize )
+	clientSocket.write data
+	clientSocket.flush
+	#puts (data)
+	return data
+end
+
+
+def forwardMessage( readSocket, writeSocket, readBufferSize )
+	data = readSocket.read( readBufferSize )
+	writeSocket.write data
+	writeSocket.flush
+	puts (data)
+	return data
+end
+
