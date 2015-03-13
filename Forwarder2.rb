7c072fa2ff706b84ff0cb60118ca4e473af2c6a3
gdabu <geoffdabu@gmail.com>
2015-03-13 14:57:11 -0700

    EPOLL Forwarder reading from config file

--- Forwarder2.rb
+++ Forwarder2.rb
@@ -16,20 +16,23 @@ module VirtualPort
     end
 end
 
-module Server 
+module Server
     attr_accessor :client
+    @ServerIP
+    @ServerPort
 
     def initialize *args
         super
+        @ServerIP = args[0]
+        @ServerPort = args[1]
     end
 
     def create_client(client)
-        puts client
         @client = client
     end
 
     def post_init
-        EM.connect "localhost", 2000, VirtualPort, self
+        EM.connect @ServerIP, @ServerPort, VirtualPort, self
     end
 
     def receive_data data
@@ -39,9 +42,38 @@ module Server
     end
 end
 
+#------------------------
+#-- Variable Declaration
+#------------------------
+begin
+    HOST = ARGV[0]
+    $configFile = ARGV[1]
+rescue Exception => argException
+    puts ">> Illegal Arguments"
+    puts ">> Usage: ruby Server.rb (serverIP configFile)"
+    exit
+end
+
+if ARGV.length != 2
+    puts ">> Illegal Arguments"
+    puts ">> Usage: ruby Server.rb (serverIP configFile)"
+    exit
+end
+
+
+#contains all the variables needed to forward a packet from one socket to another
+forwardingPairs = Array.new{Array.new(3)}
+
+File.foreach($configFile).grep /FORWARD/ do |line|
+    forwardingInfo = line.split(/[ ,:]/)
+    forwardingPairs.push([forwardingInfo[1], forwardingInfo[2], forwardingInfo[3].strip!])
+end
+
 EM.epoll
 
 EM.run {
-    puts "starting server"
-    EM.start_server "localhost", 2222, Server
+    for i in 0..forwardingPairs.length - 1 do
+        EM.start_server HOST, forwardingPairs[i][0], Server, forwardingPairs[i][1], forwardingPairs[i][2]
+        puts "Echo server listening on #{HOST}:#{forwardingPairs[i][0]}"
+    end
 }
